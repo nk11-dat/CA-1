e7b08b9f98bd2264fc80e8199ea1c8f63cb9de17
Nicki Kielsen <cph-nk11@cphbusiness.dk>
2022-09-29 14:01:02 +0200

    WC tid

---
+++ src/main/java/rest/PersonResource.java
@@ -0,0 +1,52 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.PersonDTO;
+import dtos.RenameMeDTO;
+import facades.FacadeExample;
+import facades.PersonFacade;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("person")
+public class PersonResource
+{
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+       
+    private static final PersonFacade FACADE =  PersonFacade.getInstance(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+    @Path("count")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getRenameMeCount() {
+       
+        long count = FACADE.getRenameMeCount();
+        //System.out.println("--------------->"+count);
+        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    }
+
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String createPerson(String input){
+        PersonDTO rmdto = GSON.fromJson(input, PersonDTO.class);
+        rmdto = FACADE.create(rmdto);
+        System.out.println(rmdto);
+        return GSON.toJson(rmdto);
+//        return Response.ok().entity(rmdto).build();
+    }
+}
